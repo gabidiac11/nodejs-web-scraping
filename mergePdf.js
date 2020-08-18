5829821f273d2786a6a1f85225577bfe62ec9a14
unknown <gabi@thebrandwebbers.com>
2020-08-18 22:12:55 +0300

    dummy mergin one by one algorithm

--- mergePdf.js
+++ mergePdf.js
@@ -5,6 +5,10 @@ const fs = require('fs');
 // async function recursiveMergin(subArr, subArr2, arr) {
 //     return 
 // }
+async function combineTwoPdf(first, second, output) {
+    //Save as new file
+    return PDFMerge([first, second], {output});
+}
 
 fs.readFile("localPaths.json",  async function(err, buf) {
     const files = JSON.parse(buf.toString())['localPaths'].map(
@@ -12,14 +16,14 @@ fs.readFile("localPaths.json",  async function(err, buf) {
             `${__dirname}/${link}`,
         
     );
-    
-    //Save as new file
-    PDFMerge(files, {output: `${__dirname}/merged_files.pdf`})
-    .then((buffer) => {
-        // console.log({buffer});
-    }, (err) => {
-        // console.log({err});
-    });
+    let  output = `${__dirname}/merged_76.pdf`;
+    let first = `${__dirname}/merged_74.pdf`;
+    for(let i = 75; i < files.length; i++) {
+        await combineTwoPdf(first, files[i], output);
+        first = output;
+        output = `${__dirname}/merged_${i}.pdf`;
+    }
+
 });
 
  
